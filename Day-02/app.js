673a9b1c45092b4527bd48467e4175bdd8ed8b03
KhanzaidGithub <zaidscestudent@gmail.com>
2025-01-01 03:21:27 -0800

    day 02 Done

--- Day-02/app.js
+++ Day-02/app.js
@@ -1,6 +1,25 @@
+// Problem to solve 
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+// var createCounter = function (n) {
+//     return function () {
+//         console.log(n++);
+//     };
+// };
+
+// let counter = createCounter(10)
+// counter() // 10
+// counter() // 11
+// counter() // 12
+// counter() // 13
+
 var createCounter = function(n) {
-    
     return function() {
-        
+        return n++;  // Return the current value of n and then increment it
     };
 };
+let counter = createCounter(10);
+console.log(counter()); // 10
+console.log(counter()); // 11
+console.log(counter()); // 12
