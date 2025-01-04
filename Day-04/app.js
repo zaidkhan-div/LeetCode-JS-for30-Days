8620e606637c2e9d0370b87bdfb3d6beb7e1685e
KhanzaidGithub <zaidscestudent@gmail.com>
2025-01-03 18:36:17 -0800

    Day-04 is done

--- Day-04/app.js
+++ Day-04/app.js
@@ -1,24 +1,51 @@
-var createCounter = function (init) {
-    console.log(init);
-
-    return {
-        increment: function () {
-            init++
-        },
-        decrement: function () {
-            init--
-        },
-        reset: function () {
-            init == init
-        },
-    }
-};
 
 
-const counter = createCounter(5);
-counter.increment()
-counter.increment()
+
+// var createCounter = function (init) {
+
+//     // console.log(init);
+//     let n = init;
+//     return {
+//         increment: function () {
+//             console.log(++n);
+            
+//         },
+//         reset: function () {
+//             console.log(n=init);
+//         },
+//         decrement: function () {
+//             console.log(--n);
+//         },
+      
+//     }
+// };
+
+
+// const counter = createCounter(5);
+// counter.increment()
+// counter.reset()
+// counter.decrement()
 
 // counter.increment(); // 6
 // counter.reset(); // 5
 // counter.decrement(); // 4
+
+var createCounter = function (init) {
+    let n = init;
+    return {
+        increment: function () {
+           return ++n
+        },
+        reset: function () {
+          return  n = init
+        },
+        decrement: function () {
+           return --n
+        }
+        
+    }
+};
+const counter = createCounter(5)
+console.log(counter.increment())
+console.log(counter.reset())
+console.log(counter.decrement())
