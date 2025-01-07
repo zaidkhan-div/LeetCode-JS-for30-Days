60f6390e8dd355424da19ad70cf9bd060e051054
KhanzaidGithub <zaidscestudent@gmail.com>
2025-01-07 08:13:08 -0800

    Completed the Task

--- Day-06/app.js
+++ Day-06/app.js
@@ -6,5 +6,22 @@
 
 
 var filter = function (arr, fn) {
-    let array = []
+    let filteredArr = [];
+    for (i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i])
+        }
+    }
+    return filteredArr;
 };
+
+let arr = [0, 10, 20, 30];
+fn = function greaterThan10(n) {
+    return n > 10
+}
+
+let newArray = filter(arr, fn);
+
+console.log(newArray);
+console.log('Hello world');
+
